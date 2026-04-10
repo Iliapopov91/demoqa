b4f3668ffa6f46a7ef495b41d3df1c28c2cc9685
Илья <50883558+Iliapopov91@users.noreply.github.com>
2026-04-10 14:52:17 +0300

    Исключение java.class Рефакторинг TestData + добавление генерации

--- src/test/java/tests/TestData.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class TestData {
-
-//как захардкодить тест данные
-    public static String firstName = "Egor",
-    lastName = "Gupin",
-    userEmail = "egor@gupin.com";
-}
---
+++ src/test/java/utils/TestData.java
@@ -0,0 +1,105 @@
+package utils;
+
+import com.github.javafaker.Faker;
+
+import java.security.SecureRandom;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static utils.RandomUtils.getRandomInt;
+
+public class TestData {
+
+    Faker faker = new Faker(new Locale("en-GB"));
+
+    public String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            streetAddress = faker.address().streetAddress(),
+            invalidPhoneNumber = "99999999",
+            randomString = getRandomString(2),
+            gender = getRandomGender(),
+            phoneNumber = getRandomPhone(),
+            hobbies = getRandomHobbies(),
+            day = getRandomDay(),
+            month = getRandomMonth(),
+            year = getRandomYear(),
+            subjects = getRandomSubjects(),
+            state = getRandomState(),
+            city = getRandomCity();
+
+    public static String getRandomString(int len) {
+        //String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+
+        return sb.toString();
+    }
+
+//    public static String getRandomPhone () {
+//        return String.format("+%s (%s) %s - %s - %s", getRandomInt(1, 9), getRandomInt(111, 999),
+//                getRandomInt(111, 999), getRandomInt(11, 99), getRandomInt(11, 99));
+    public String getRandomPhone() {
+    long num = ThreadLocalRandom.current().nextLong(9_000_000_000L, 10_000_000_000L);
+    return Long.toString(num);
+    }
+
+    public static String getRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+
+        return getRandomItemArray(genders);
+    }
+
+    public static String getRandomHobbies() {
+        String[] hobbies = {"Sports", "Reading", "Music"};
+
+        return getRandomItemArray(hobbies);
+    }
+
+    public static String getRandomSubjects() {
+        String[] subjects = {"English", "Chemistry", "Computer Science",
+                "Commerce", "Accounting", "Economics",
+                "Social Studies", "Civics"};
+
+        return getRandomItemArray(subjects);
+    }
+    public String getRandomDay() {
+        return String.valueOf(faker.number().numberBetween(1, 28));
+    }
+
+    public String getRandomMonth() {
+        String[] month = {"December", "January", "February", "March",
+                "April", "May", "June", "July",
+                "August", "September", "October", "November"};
+        return faker.options().option(month);
+    }
+
+    public String getRandomYear() {
+        return String.valueOf(faker.number().numberBetween(1950, 2025));
+    }
+
+    public String getRandomState() {
+        String[] state = new String[]{faker.options()
+                .option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan")};
+       // {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+
+        return getRandomItemArray(state);
+    }
+
+    public String getRandomCity(){
+        if (state.equals("NCR")) city = faker.options().option("Delhi", "Gurgaon", "Noida");
+        if (state.equals("Uttar Pradesh")) city = faker.options().option("Agra", "Lucknow", "Merrut");
+        if (state.equals("Haryana")) city = faker.options().option("Karnal", "Panipat");
+        if (state.equals("Rajasthan")) city = faker.options().option("Jaipur", "Jaiselmer");
+        return city;
+    }
+    //обработка массива значений (сейчас используется в связке с полом, но можно и другие массивы)
+    public static String getRandomItemArray(String[] array) {
+        int index = getRandomInt(0, array.length - 1);
+
+        return array[index];
+    }
+}
