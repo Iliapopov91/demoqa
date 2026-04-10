b0c020691d4302d13c5a91ca2135a8f5d0341333
Илья <50883558+Iliapopov91@users.noreply.github.com>
2026-04-10 15:33:53 +0300

    updated CalendarComponent.java

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,13 +1,13 @@
 package pages.components;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
 
-    public void setDate(String day, String month, String year) {
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOption("1991");
-        $(".react-datepicker__month-select").selectOption("August");
-        $(".react-datepicker__day.react-datepicker__day--008").click();
+public void setDate(String day, String month, String year) {
+    $(".react-datepicker__year-select").selectOptionByValue(year);
+    $(".react-datepicker__month-select").selectOption(month);
+    $("#dateOfBirth").$(".react-datepicker__month").find(byText(day)).click();
     }
 }
